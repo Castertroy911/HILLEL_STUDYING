3b8a7ee7f0f431bb3fdc353046a232986dcd3cb2
Dima Leonov <leonovdima1998@gmail.com>
2023-04-29 00:02:36 +0300

    Write a calculator code

---
+++ Lesson 5/calculator_v1.1.py
@@ -0,0 +1,17 @@
+class FormulaError(ValueError):
+    pass
+
+
+try:
+    first_value, operand, second_value = input('Enter the formula: ').split()
+    first_value = float(first_value)
+    second_value = float(second_value)
+except ValueError:
+    raise FormulaError(f'Check the entered formula') from None
+if operand != '+' and operand != '-':
+    raise FormulaError(f'Operand should be "+" or "-". You enter "{operand}"')
+elif operand == '+':
+    print(first_value + second_value)
+else:
+    print(first_value - second_value)
+
