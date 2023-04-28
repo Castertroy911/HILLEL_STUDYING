c9d498caa6cc4e2aafdf675e0f0751e22b5c5f5a
Dima Leonov <leonovdima1998@gmail.com>
2023-04-29 00:01:44 +0300

    Refactor code and add some functions

--- Lesson 5/second_degree_equation.py
+++ Lesson 5/second_degree_equation.py
@@ -1,20 +1,40 @@
-a, b, c = (int(i) for i in input().split())
+# Создал свой ексепшн по примеру их лекции
+class DiscriminantError(Exception):
+    pass
+
+
+# Зациклил ввод данных до тех пор, пока не будет введены только числа
+try:
+    a, b, c = (int(i) for i in input('Please, enter numbers: ').split())
+    err = False
+except ValueError:
+    err = True
+    while err:
+        try:
+            a, b, c = (int(i) for i in input('Please, enter only numbers: ').split())
+            err = False
+        except ValueError:
+            err = True
 
 DISCRIMINANT = b ** 2 - 4 * a * c
 
 
-class DiscriminantError(Exception):
-    def __init__(self, message):
-        self.message = message
-
-    def __str__(self):
-        return self.message
+# Пара функций, чтобы не использовать повторно одинаковые формулы
+def first_x(a, b):
+    x_1 = (-b + DISCRIMINANT ** 0.5) / (2 * a)
+    return x_1
 
 
+def second_x(a, b):
+    x_2 = (-b - DISCRIMINANT ** 0.5) / (2 * a)
+    return x_2
+
+
+# Реализация задания 
 if DISCRIMINANT > 0 and a != 0:
-    print(f'x1 = {(-b + DISCRIMINANT ** 0.5) / (2 * a)}')
-    print(f'x2 = {(-b - DISCRIMINANT ** 0.5) / (2 * a)}')
+    print(f'x1 = {int(first_x(a, b))}')
+    print(f'x2 = {int(second_x(a, b))}')
 elif DISCRIMINANT == 0:
-    print(f'x = {(-b + DISCRIMINANT ** 0.5) / (2 * a)}')
+    print(f'x = {int(first_x(a, b))}')
 else:
     raise DiscriminantError(f'Discriminant < 0, so there are no correct answers')
