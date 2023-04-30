0b05a1bfb878d17f68f579a7490938f4149dc699
Dima Leonov <leonovdima1998@gmail.com>
2023-04-30 11:49:18 +0300

    Add function and logic for the second home rask

---
+++ Lesson 6/square.py
@@ -0,0 +1,16 @@
+err = True
+while err:
+    try:
+        entered_value = int(input('Please, enter the number value: '))
+        err = False
+    except ValueError:
+        print('You need to enter only number, not letters or symbols!')
+        err = True
+
+
+def square(value):
+    perimeter = value * 4
+    square_area = value ** 2
+    diagonal = 2 ** 0.5 * value
+    return perimeter, square_area, diagonal
+
