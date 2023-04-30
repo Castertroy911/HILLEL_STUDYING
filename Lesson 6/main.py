fced0dca75f48caf036e5501c0950d347ea37acd
Dima Leonov <leonovdima1998@gmail.com>
2023-04-30 14:35:38 +0300

    Add imports and call functions from the other files

---
+++ Lesson 6/main.py
@@ -0,0 +1,14 @@
+from change_list import change_list
+from is_prime import is_prime
+from lst_to_dict import to_dict
+from power_of_two import power_of_two
+from square import square
+from sum_range import sum_range
+
+if __name__ == '__main__':
+    print(power_of_two(8))
+    print(square(2))
+    print(is_prime(867))
+    print(change_list(['first element', 'second element', 'third element', 'last element']))
+    print(to_dict(['first element', 'second element', 'third element', 'last element']))
+    print(sum_range(3, 1))
